6e0483f89e9615200e59c571910cae206be1dfab
Alixander Roden <S190762@uos.ac.uk>
2018-04-30 08:12:14 +0100

    added latest build and latest files

--- EmergingTechnologies/GFXBenchmark/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Application.h"
-
-int main(int argc, char** argv)
-{
-	application::pre_init(1024, 768, graphics_api::vulkan);
-	application::get_instanced()->run();
-	application::destroy();
-	return 1;
-}
